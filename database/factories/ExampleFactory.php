7b64367f0d874c7cbfc8d3f411439b778e688c0b
HiroyukiMakita <makita@j-yado.com>
2021-08-04 09:31:14 +0900

    add: テストデータ登録のための Factory と Seeder を追加

---
+++ database/factories/ExampleFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var Factory $factory */
+
+use App\Example;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(
+    Example::class,
+    function (Faker $faker) {
+        return [
+            'string' => $faker->name,
+            'integer' => $faker->randomNumber(),
+            'text' => $faker->text(100),
+        ];
+    }
+);
--- database/seeds/ExamplesTableSeeder.php
+++ database/seeds/ExamplesTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ExamplesTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run(): void
     {
-         $this->call(ExamplesTableSeeder::class);
+        factory(App\Example::class, 5)->create();
     }
 }
