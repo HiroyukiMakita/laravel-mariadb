796169a08848bb22dd9801ca1ab1da26f89ce919
HiroyukiMakita <makita@j-yado.com>
2021-08-23 16:55:05 +0900

    update: 暗号化に対応するようにユーザーテーブルの名前とメールアドレスの文字数を増やす

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            /**
+             * https://yamaback.hatenadiary.org/entry/20110819/1313735667
+             * https://qiita.com/cszunwr291/items/f5fba3fc6ac428f7fabb
+             * 最大文字数（STR_LENGTH）を暗号化後の文字数の計算式
+             * 16 * (trunc(STR_LENGTH / 16) + 1) * 2 + 16 * 2
+             * [name]  max: 100 => 256
+             * [email] max: 254 => 544
+             */
+            $table->string('name', 300);
+            $table->string('email', 600);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedTinyInteger('role');
